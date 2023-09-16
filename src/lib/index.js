3d867faa67ace2df854a8409cf3f52442456443d
Djole <0xdjole@gmail.com>
2023-09-17 00:48:48 +0200

    add misisng export

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,6 +4,6 @@ export { default as Screen } from './components/Screen/index.svelte';
 export { default as Stack } from './components/Stack/index.svelte';
 export { default as Tab } from './components/Tab/index.svelte';
 export { default as TabView } from './components/TabView/index.svelte';
+export { default as NavigationBar } from './components/NavigationBar.svelte';
 
 export { navigation } from './store';
-
