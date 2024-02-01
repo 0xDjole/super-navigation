bcd7f050470341f2d6e46d32232230a1f22c88aa
Djole <0xdjole@gmail.com>
2024-02-01 22:18:04 +0100

    sn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,34 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	parser: '@typescript-eslint/parser', // Specifies the ESLint parser for TypeScript
 	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		sourceType: 'module'
 	},
 	env: {
 		browser: true,
-		es2017: true,
+		es2021: true,
 		node: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		'plugin:svelte/recommended',
+		'prettier' // Enables eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+	],
+	plugins: [
+		'@typescript-eslint',
+		'svelte',
+		'prettier' // Runs Prettier as an ESLint rule and reports differences as individual ESLint issues
+	],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte/svelte'
+		}
+	],
+	rules: {
+		'prettier/prettier': 'error' // Add this to turn prettier errors into ESLint errors
+		// Add any custom rules or overrides here
 	}
 };
--- src/routes/Back.svelte
+++
@@ -1,1 +0,0 @@
-<div class="font-bold text-xl text-white">Back 2</div>
